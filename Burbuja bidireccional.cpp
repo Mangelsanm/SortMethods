314c6946435e0d1c695546f1e6ffce738383ce06
Miguel <mares112358@gmail.com>
2018-10-04 07:11:19 -0500

    bidireccional actualizado, gitignore actualizado

--- Burbuja bidireccional.cpp
+++ Burbuja bidireccional.cpp
@@ -12,20 +12,29 @@ int main(){
         cin >> Elements[i];
     }
 
-    for(int j = 0; j < nElementos - 1; j++){
-        if(Elements[j] > Elements[j+1]){
-            int aux = Elements[j];
-            Elements[j] = Elements[j+1];
-            Elements[j+1] = aux;
+/* inicia metodo de ordenamiento */
+    int izquierda = 0;
+    int derecha = nElementos - 1;
+    while(izquierda <= derecha){
+        for(int j = izquierda; j < derecha; j++){
+            if(Elements[j] > Elements[j+1]){
+                int aux = Elements[j];
+                Elements[j] = Elements[j+1];
+                Elements[j+1] = aux;
+            }
         }
-    }
-    for(int k = nElementos - 1; k > 0; k--){
-        if(Elements[k] < Elements[k-1]){
-            int aux = Elements[k];
-            Elements[k] = Elements[k-1];
-            Elements[k-1] = aux;
+
+        for(int k = derecha; k > izquierda; k--){
+            if(Elements[k] < Elements[k-1]){
+                int aux = Elements[k];
+                Elements[k] = Elements[k-1];
+                Elements[k-1] = aux;
+            }
         }
+        izquierda++;
+        derecha--;
     }
+/* fin del metodo */
 
     for(int i = 0; i < nElementos; i++){
         cout << Elements[i] << ", " << flush;
